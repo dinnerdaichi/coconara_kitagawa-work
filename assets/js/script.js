9d338f2ae43a1d3a88cb34f09a99958b91f0cc3e
rinarinas2 <skgcrn2525@gmail.com>
2025-07-06 20:19:55 +0900

    諸々設定　編集

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// サムネイル
+const sliderThumbnail = new Swiper(".slider-thumbnail", {
+  slidesPerView: 3, // サムネイルの枚数
+});
+// スライダー
+const slider = new Swiper(".slider", {
+  loop: true,
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: sliderThumbnail,
+  },
+});
